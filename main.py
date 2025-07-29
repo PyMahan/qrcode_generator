c308139a8266c6de2bab078cad30176bf7804876
PyMahan <76549686+PyMahan@users.noreply.github.com>
2025-07-29 16:59:58 +0330

    Updated main.py
    A few features added

--- main.py
+++ main.py
@@ -6,18 +6,44 @@ Created on Mon Feb 20 00:43:54 2023
 @author: amir
 """
 from qrcode_generator import get_string,create_qrcode,save_qrcode
+import time
+import os
 
 
 class Handler:
+
+    def get_string_2():
+        text = input(str("ENTER THE STRING FOR CONVERT TO THE QRCODE (PRESS ESC AND ENTER TO EXIT) :"))
+        return text
+
     
     @staticmethod
-    def run():
-        name = get_string()
+    def run(get=get_string):
+        name = get()
         qr = create_qrcode(string=name)
         try:
-            save_qrcode(filename=name[0:20],qrcode=qr)
+            os.mkdir("Saved")
         except:
-            save_qrcode(filename='file',qrcode=qr)
+            pass
+        try:
+            save_qrcode(filename=f"Saved/{name[0:20]}",qrcode=qr)
+        except:
+            save_qrcode(filename="Default",qrcode=qr)
+            
+        finalTxt = "QRCode created!"
+        print("\n".center(55," "),end="",flush=True)
+        for char in finalTxt:
+            print(char,end="",flush=True)
+            time.sleep(0.05)
+        print("\n")
+        Handler.retry()
+    def retry():
+        Handler.run(Handler.get_string_2)
+
+
+
             
 if __name__ == "__main__":
     Handler.run()
+    
+
